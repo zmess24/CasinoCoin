7812b8b40a767892110102a6e105c8a981a3270d
zmess24 <zac.messinger@gmail.com>
2018-02-09 18:52:07 -0500

    closed meta tag

--- components/Layout.js
+++ components/Layout.js
@@ -13,7 +13,7 @@ export default props => {
                 <meta name="viewport" content="initial-scale=1.0, width=device-width" />
                 <meta name="author" content="Zac Messinger"/>
                 <meta name="keywords" content="Ethereum, DAPP, Zac, Messinger, zmess, zmess24, Ether"/>
-                <meta http-equiv="content-type" content="text/html;charset=UTF-8">
+                <meta http-equiv="content-type" content="text/html;charset=UTF-8"/>
 
                 <meta name="description" content="Decentralized application built on Ethereum using the Rinkeby Network that allows users to bet on a number to win Ether."/>
                 <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.12/semantic.min.css"></link>
